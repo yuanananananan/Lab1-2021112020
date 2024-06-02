0ef6024ed522e6f839a83269c954eea0e476eb09
yuanananananan <2754638412@qq.com>
2024-06-02 12:09:20 +0800

    Initial commit for Lab1 project

---
+++ lab1/directed-graph-backend/src/test/java/com/example/directedgraphbackend/DirectedGraphBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.directedgraphbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DirectedGraphBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
